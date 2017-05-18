a24e2cba0dbb051b9697d300b33a1a11dcef448a
Brandon Donohue <pisquad@yahoo.com>
2017-05-18 04:44:45 +0200

    some errors, but it compiles

--- main.cpp
+++ main.cpp
@@ -43,22 +43,35 @@ int main(){
         PokemonTypeChart[addTypeName] = addType;
     }
     infile.close();
-    /*
-    map<array<PokemonType,2>,int> PokemonTypeValue ({{}});
-    PokemonType attack, defense;
-    PokemonType insertTypeValue[2];
-    for(int attackType = normal; attackType <= fairy; attackType++){
-        attack = static_cast<PokemonType>(attackType);
-        insertTypeValue[0] = attack;
-        for(int defenseType = normal; defenseType <= fairy; defenseType++){
-            defense = static_cast<PokemonType>(defenseType);
-            insertTypeValue[1] = defense;
-            cout << PokemonTypeString[insertTypeValue[0]] << " " << PokemonTypeString[insertTypeValue[1]] << endl;
-            //PokemonTypeValue[insertTypeValue] = 1;
-            //PokemonTypeValue.insert(pair<array<PokemonType,2>,int>(insertTypeValue,1));
+
+    string input, input2;
+    int cut;
+    Type currentType;
+    map<string,PokemonType>::iterator it;
+    while(1){
+        cout << "Input type: " << endl;
+        cin;
+        getline(cin,input);
+        if(input == "quit"){
+            break;
+        }
+        if(input.find(" ") != std::string::npos){
+            cut = input.find(" ") != std::string::npos;
+            cout << "two word" << endl;
+        } else {
+            it = PokemonStringToType.find(input);
+            if (it != PokemonStringToType.end()){
+                currentType = PokemonTypeChart[PokemonStringToType[input]];
+                cout << "Type Chart against " << input << ":" << endl;
+                for(int typeCounter = normal; typeCounter <= fairy; typeCounter++){
+                    cout << PokemonTypeToString[static_cast<PokemonType>(typeCounter)] << " ";
+                    cout << currentType.getMultiplier(static_cast<PokemonType>(typeCounter)) << endl;
+                }
+            } else {
+                cout << "not a type" << endl;
+            }
         }
     }
-    */
     return 0;
 }
 
